fc308d74b0f8aacdb7642519e72a56cd8c027ff2
nerumoa019 <50538677+nerumoa019@users.noreply.github.com>
2021-08-07 02:21:57 +0900

    無限ジャンプ可能

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -9,21 +9,25 @@ public class PlayerManager : MonoBehaviour
     private float xSpeed;
     private float xScale;
     private float jumpPower = 950f;
-    private float time;
-    private bool pressJump = false;
+    private float waitTime;
+    private float time2;
+    private float time3;
+    private bool isJump = false;
+    private bool canJump = false;
+    private bool autoJump = false;
     private Animator animator;
     private Rigidbody2D rb;
     private Vector2 vector;
-
-    private bool pressP = false;
-    private bool isJump = false;
-
 
 
     private void Start()
     {
         rb       = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        waitTime = 0.70588235294f;
+        time2 = 0.35f;
+        time3 = waitTime - time2;
     }
 
     private void Update()
@@ -32,15 +36,6 @@ public class PlayerManager : MonoBehaviour
         xScale = Mathf.Sign(transform.localScale.x);
 
         //Debug.DrawLine(transform.position - (transform.right * 0.18f * xScale) - transform.up * 1.85f, transform.position - (transform.right * 0.18f * xScale) - transform.up * 1.95f, Color.red);
-
-        /*
-        // ジャンプ判定
-        if (Input.GetKeyDown("space")) {
-            pressJump = true;
-        } else if (Input.GetKeyUp("space")) {
-            pressJump = false;
-        }
-        */
 
         // アニメーション遷移
         animator.SetFloat("Speed", Mathf.Abs(xSpeed));
@@ -51,15 +46,17 @@ public class PlayerManager : MonoBehaviour
         }
 
         // ジャンプ判定
-        if (Input.GetKeyDown("space") && !pressJump) {
-            StartCoroutine("AutoJump");
-            pressJump = true;
+        if (Input.GetKeyDown("space") && !autoJump) {
+            StartCoroutine("AutoJumpStart");
+            autoJump = true;
         }
 
         // デバッグ用　のけぞり
         if (Input.GetKeyDown(KeyCode.U)) {
             animator.SetTrigger("Miss");
         }
+
+
     }
 
     private void FixedUpdate()
@@ -81,20 +78,22 @@ public class PlayerManager : MonoBehaviour
         vector.x = xSpeed * 5f;
         vector.y = rb.velocity.y;
 
-        // デバッグ用 Pボタン
-        if (pressP && HitGround()) {
-            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Force);
-        } else if (pressP && !HitGround()) {
-            pressP = false;
-            isJump = true;
-        }
+        // オートジャンプ処理
+        if (autoJump) {
+            if (canJump && HitGround()) {
+                rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Force);
+            } else if (canJump && !HitGround()) {
+                canJump = false;
+                isJump = true;
+            }
 
-        if (isJump && HitGround()) {
-            xSpeed = 0f;
-            isJump = false;
-        } else if (isJump) {
-            xSpeed = Mathf.Sign(transform.localScale.x);
-            vector.x = xSpeed * 7.6f;
+            if (isJump && HitGround()) {
+                xSpeed = 0f;
+                isJump = false;
+            } else if (isJump) {
+                xSpeed = Mathf.Sign(transform.localScale.x);
+                vector.x = xSpeed * 7.6f;
+            }
         }
 
         // 移動処理
@@ -111,83 +110,35 @@ public class PlayerManager : MonoBehaviour
         animator.SetTrigger("Rythem");
     }
 
-    /*
-    public void AutoJumpa()
+    IEnumerator AutoJumpStart()
     {
+        for (int i = 0; i < 3; i++) {
+            canJump = true;
+            if (i == 2) {
+                yield return new WaitForSeconds(time2);
+                transform.localScale = new Vector2(transform.localScale.x * -1f, 1.25f);
+                yield return new WaitForSeconds(time3);
+                break;
+            }
+            yield return new WaitForSeconds(waitTime);
+        }
+
         StartCoroutine("AutoJump");
     }
-    */
 
     IEnumerator AutoJump()
     {
-        time = 0.70588235294f;
         for (int i = 0; i < 4; i++) {
-            pressP = true;
+            canJump = true;
             if (i == 3) {
-                time = 0.35f;
-                yield return new WaitForSeconds(time);
-
+                yield return new WaitForSeconds(time2);
                 transform.localScale = new Vector2(transform.localScale.x * -1f, 1.25f);
-                time = 0.70588235294f - 0.35f;
+                yield return new WaitForSeconds(time3);
+                break;
             }
-            yield return new WaitForSeconds(time);
+            yield return new WaitForSeconds(waitTime);
         }
 
-        pressJump = false;
-
-        /*
-        time = 0.70588235294f;
-        for (int i = 0; i < 4; i++) {
-            pressP = true;
-            if (i == 3) {
-                time = 0.35f;
-                yield return new WaitForSeconds(time);
-
-                transform.localScale = new Vector2(transform.localScale.x * -1f, 1.25f);
-                time = 0.70588235294f - 0.35f;
-            }
-            yield return new WaitForSeconds(time);
-        }
-
-        time = 0.70588235294f;
-        for (int i = 0; i < 4; i++) {
-            pressP = true;
-            if (i == 3) {
-                time = 0.35f;
-                yield return new WaitForSeconds(time);
-
-                transform.localScale = new Vector2(transform.localScale.x * -1f, 1.25f);
-                time = 0.70588235294f - 0.35f;
-            }
-            yield return new WaitForSeconds(time);
-        }
-
-        time = 0.70588235294f;
-        for (int i = 0; i < 4; i++) {
-            pressP = true;
-            if (i == 3) {
-                time = 0.35f;
-                yield return new WaitForSeconds(time);
-
-                transform.localScale = new Vector2(transform.localScale.x * -1f, 1.25f);
-                time = 0.70588235294f - 0.35f;
-            }
-            yield return new WaitForSeconds(time);
-        }
-
-        time = 0.70588235294f;
-        for (int i = 0; i < 4; i++) {
-            pressP = true;
-            if (i == 3) {
-                time = 0.35f;
-                yield return new WaitForSeconds(time);
-
-                transform.localScale = new Vector2(transform.localScale.x * -1f, 1.25f);
-                time = 0.70588235294f - 0.35f;
-            }
-            yield return new WaitForSeconds(time);
-        }
-        */
-
+        StartCoroutine("AutoJump");
     }
 }
