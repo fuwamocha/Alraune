81aaeaa8c1ef2ea874a93c72fb5c73890ef4ef7a
nerumoa019 <50538677+nerumoa019@users.noreply.github.com>
2021-08-07 16:45:53 +0900

    タイミング修正

--- Assets/Scripts/PlayerSE.cs
+++ Assets/Scripts/PlayerSE.cs
@@ -15,9 +15,11 @@ public class PlayerSE : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        /*
         if(Input.GetKeyDown(KeyCode.Space)){
         audioSource.PlayOneShot(shan);
         }
+        */
     }
 }
