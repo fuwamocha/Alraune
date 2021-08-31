ee277d961f20875e1b63a19429476d89b29179c2
nerumoa019 <50538677+nerumoa019@users.noreply.github.com>
2021-09-01 08:21:52 +0900

    微弱な時間のずれの解消
    ゲーム実行ごとに曲の再生が0.02～0.04秒程ずれていましたが、これは時間の取り方による問題だったため修正しました

--- Assets/Scripts/nerumoa/DebugTime.cs
+++ Assets/Scripts/nerumoa/DebugTime.cs
@@ -8,33 +8,14 @@ public class DebugTime : MonoBehaviour
     [SerializeField] RythemManager rythem = default;
     [SerializeField] Text timeText = default;
 
-    /*
-    private int sec;
-    private float dec;
-    private float totalTime = 0f;
-    */
-
-    private bool isCount = true;
     public bool isChecked;
 
     private void Update()
     {
-        /*
-        if (isCount) {
-            totalTime += Time.deltaTime;
-            timeText.text = totalTime.ToString("N2");
-        }
-        */
-
         if (isChecked) {
-            timeText.text = rythem.elaspedTime.ToString("N2");
+            timeText.text = rythem.aTime.ToString("N9");
         } else {
-            timeText.text = rythem.aTime.ToString("N4");
+            timeText.text = rythem.bTime.ToString("N9");
         }
-    }
-
-    public void CountStop()
-    {
-        isCount = false;
     }
 }
--- Assets/Scripts/nerumoa/RythemManager.cs
+++ Assets/Scripts/nerumoa/RythemManager.cs
@@ -9,42 +9,43 @@ public class RythemManager : MonoBehaviour
     [SerializeField] GameObject BGMs = default;
     [SerializeField] GameObject SEs = default;
 
-    public double totalTime = 0d;          // 合計経過時間
-    private double initTime;                // 初期時間
-    public double elaspedTime = 0d;             // 経過時間
-    private double bufferTime;              // 緩衝時間 (SE用)
+    private double totalTime = 0d;          // トータル経過時間 (sec)
+    private double elaspedTime;             // 1回毎の経過時間 (sec)
+    private double bufferTime;              // 緩衝時間 (タイミングの同期用)
     private double bpm170 = 120 / 170d;
-    private double delay = 0.01d;
-    private bool cooldown = false;
+    private bool cooldown = false;          // 連続実行の防止用
 
     private AudioSource audio_BGM;
     private AudioSource audio_SE;
 
     public double aTime;
+    public double bTime;
 
     private void Start()
     {
         audio_BGM = BGMs.GetComponent<AudioSource>();   // BGMの管理
         audio_BGM.clip = BGM[0];
-        audio_BGM.PlayScheduled(delay);
+        audio_BGM.Play();
         audio_SE = SEs.GetComponent<AudioSource>();     // SEの管理
         audio_SE.clip = SE[0];
 
-        initTime = AudioSettings.dspTime - delay;
-        bufferTime = bpm170 * 1000d * 0.92;
+        bufferTime = bpm170 * 0.92;
     }
 
     private void FixedUpdate()
     {
-        totalTime = AudioSettings.dspTime - initTime;   // トータル経過時間 (s)
-        elaspedTime = totalTime % bpm170 * 1000d;       // 1回毎の経過時間 (ms)
+        totalTime = audio_BGM.time;
+        elaspedTime = totalTime % bpm170;
 
+        /* 適正タイミングの算出・実行 */
         if (elaspedTime >= bufferTime) {
 
             if (!cooldown) {
-                audio_SE.PlayScheduled((bpm170 * 1000d - elaspedTime) / 1000d);
-                aTime = (bpm170 * 1000d - elaspedTime) / 1000d;
+                audio_SE.PlayScheduled(bpm170 - elaspedTime);
                 cooldown = true;
+
+                aTime = totalTime + (bpm170 - elaspedTime);     // デバッグ用 (曲の経過時間)
+                bTime = aTime % bpm170;                         // デバッグ用
             }
         } else if (cooldown) {
             cooldown = false;
