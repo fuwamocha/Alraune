6e000320cbf04a28af2d5db396e700271848184e
fuwamocha <74049905+fuwamocha@users.noreply.github.com>
2022-03-09 00:26:30 +0900

    BGMの値を参照するように

--- Assets/Scripts/System/Config.cs
+++ Assets/Scripts/System/Config.cs
@@ -4,7 +4,7 @@ public class Config
     internal static int BPM = 170;
 
     // 一拍分の秒数
-    internal static float SecondsPerBeat = 60 / 170f;
+    internal static float SecondsPerBeat = 60f / BPM;
     internal static float StepSecondsPerBeat = SecondsPerBeat * 2;
 
     internal enum GameStatus
